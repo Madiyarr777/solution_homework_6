b4f76c628e2c3a5df4bc92f8f49f6e3aa8a83655
Madiyar Tankayev <madiyar.tankayev@narxoz.kz>
2025-04-14 22:15:55 +0500

    Uploaded a new file

---
+++ src/commandpattern/TurnOffLightCommand/TurnOffLightCommand.java
@@ -0,0 +1,20 @@
+package commandpattern;
+
+
+public class TurnOffLightCommand implements Command {
+    private Light light;
+
+    public TurnOffLightCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.off();
+    }
+
+    @Override
+    public void undo() {
+        light.on();
+    }
+}
